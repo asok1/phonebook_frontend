058dcb28464da3e263da593fa3cf97b5aa23ccfb
asok1 <sokolovalex4@gmail.com>
2023-04-12 21:33:11 -0500

    env handling

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-const baseUrl = process.env.REACT_APP_backend_base_url
+const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL
 
 const getAll = () => {
+  console.log('env', process.env.NODE_ENV)
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
